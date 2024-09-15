7cc1f71026ae448ecb677029fa293bc5dbad74fc
pedro-hdias <pedrohenriquediassousa.2000@gmail.com>
2024-09-15 16:55:43 -0300

    Refatorar função de exibição do menu e obtenção da opção do usuário em interface.py

--- app/src/views/interface.py
+++ app/src/views/interface.py
@@ -1,26 +1,28 @@
 import logging
 from utils.helpers import get_menu
+
 menu_options = get_menu()
+log_interface = logging.getLogger('interface')
 
 def show_menu():
-    logging.debug("Exibindo o menu.")
+    log_interface.debug("Exibindo o menu.")
     print("""\n===== Sistema de Gerenciamento de Fila de Decolagem =====\n
           Selecione uma opção:""")
     for key, value in menu_options.items():
-        logging.debug(f"{key}. {value}")
+        log_interface.debug(f"{key}. {value}")
         print(f"{key}. {value}")
 
 def get_user_option():
     try:
-        logging.debug("Obtendo a opção do usuário.")
+        log_interface.debug("Obtendo a opção do usuário.")
         option = int(input("Digite o número da opção desejada: "))
         if option < 0 or option > 6:
-            logging.warning(f"Opção inválida: {option}")
+            log_interface.warning(f"Opção inválida: {option}")
             print(f"Opção inválida! Por favor, insira um dos números exibidos no menu.")
             return -1
-        logging.debug(f"Opção escolhida: {option}")
+        log_interface.debug(f"Opção escolhida: {option}")
         return option
     except ValueError:
-        logging.warning(f"Opção inválida: {option}.")
+        log_interface.warning("Opção inválida. Não foi inserido um número.")
         print("Opção inválida! Por favor, insira um número.")
         return -1
