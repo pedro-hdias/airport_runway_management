f5aa420e80d3948b94222346474f92eb3a0c78e6
pedro-hdias <pedrohenriquediassousa.2000@gmail.com>
2024-09-15 18:59:38 -0300

    Refatorar função de exibição do menu e obtenção da opção do usuário em interface.py

--- app/src/controllers/takeoff_queue.py
+++ app/src/controllers/takeoff_queue.py
@@ -2,53 +2,69 @@ from collections import deque
 from models.aircraft import Aircraft
 import logging
 
+log_takeoff_queue = logging.getLogger('takeoff_queue')
 class TakeoffQueue:
     """
-    Class that manages the aircraft takeoff queue.
+        Class that manages the aircraft takeoff queue.
     """
 
     def __init__(self):
         self.queue = deque()
 
-    def add_aircraft(self, aircraft):
-        logging.debug(f"Adicionando avião à fila de decolagem: {aircraft}")
-        self.queue.append(aircraft)
+    def add_flight(self):
+        log_takeoff_queue .info("Adicionando um voo à fila de decolagem.")
+        flight  = Aircraft.create_aircraft()
+        log_takeoff_queue .debug(f"Adicionando avião à fila de decolagem: {flight}")
+        self.queue.append(flight)
 
-    def takeoff_aircraft(self):
+    def takeoff_flight(self):
+        log_takeoff_queue .info("Decolando um voo.")
         if self.queue:
-            logging.debug(f"Decolando o voo: {self.queue[0]}")
+            log_takeoff_queue .debug(f"Decolando o voo: {self.queue[0]}")
             return self.queue.popleft()
         else:
-            logging.warning("Fila de decolagem vazia.")
+            log_takeoff_queue .warning("Fila de decolagem vazia.")
+            print("Não há voos na fila de decolagem.")
             return None
 
     def get_queue_size(self):
-        logging.debug(f"Tamanho da fila de decolagem: {len(self.queue)}")
+        log_takeoff_queue .info("Obtendo o tamanho da fila de decolagem.")
+        print(f"Total de voos na fila de decolagem: {len(self.queue)}")
+        log_takeoff_queue .debug(f"Tamanho da fila de decolagem: {len(self.queue)}")
         return len(self.queue)
 
-    def list_all_aircraft(self):
-        logging.debug(f"Listando todos os aviões na fila de decolagem. {list(self.queue)}")
+    def get_list_all_aircraft(self):
+        log_takeoff_queue .info("Listando todos os aviões na fila de decolagem.")
         if len(self.queue) == 0:
             print("A fila de decolagem está vazia.")
-            logging.warning("Tentativa de listar aviões com fila vazia.")
-            return []
-
-        return list(self.queue)
+            log_takeoff_queue .warning("Tentativa de listar aviões com fila vazia.")
+            return None
+        else:
+            print("\n=== Voos na Fila de Decolagem ===")
+            for idx, flight in enumerate(self.queue, start=1):
+                print(f"{idx}. {flight}")
 
     def get_next_aircraft(self):
+        log_takeoff_queue .info("Obtendo o próximo avião na fila de decolagem.")
         if self.queue:
-            logging.debug(f"Próximo avião na fila: {self.queue[0]}")
+            log_takeoff_queue .debug(f"Próximo avião na fila: {self.queue[0]}")
+            print(f"Próximo avião a decolar: {self.queue[0]}")
             return self.queue[0]
         else:
-            logging.warning("Fila de decolagem vazia.")
+            log_takeoff_queue .warning("Fila de decolagem vazia.")
+            print("Não há voos na fila de decolagem.")
             return None
 
-    def find_aircraft_position(self, flight_number):
-        logging.debug(f"Procurando avião com o número de voo {flight_number} na fila.")
+    def get_aircraft_position(self):
+        log_takeoff_queue .info(f"Procurando avião com o número de voo {flight_number} na fila.")
+        flight_number= input("Digite o número do voo: ").strip()
+        log_takeoff_queue .debug(f"Procurando avião com o número de voo {flight_number} na fila.")
         for index, aircraft in enumerate(self.queue):
             if aircraft.flight_number == flight_number:
-                logging.debug(f"Avião com o número de voo {flight_number} encontrado na posição {index + 1}.")
+                log_takeoff_queue .debug(f"Avião com o número de voo {flight_number} encontrado na posição {index + 1}.")
+                print(f"O avião com o número de voo {flight_number} está na posição {index + 1} da fila.")
                 return index + 1
         print(f"Avião com o número de voo {flight_number} não encontrado na fila.")
-        logging.warning(f"Avião com o número de voo {flight_number} não encontrado na fila.")
+        log_takeoff_queue .warning(f"Avião com o número de voo {flight_number} não encontrado na fila.")
         return -1
+
--- app/src/utils/helpers.py
+++ app/src/utils/helpers.py
@@ -1,83 +1,10 @@
 import logging, datetime
+from controllers.takeoff_queue import TakeoffQueue
 
-
-def validate_input(model, airline, departure, arrive, flight_number):
-    if not model:
-
-        logging.error("Modelo do avião vazio.")
-
-        print("O modelo do avião não pode ser vazio.")
-
-        return False
-
-    logging.debug(f"Modelo do avião: {model}")
-    if  not airline:
-
-        logging.error("Empresa aérea vazia.")
-
-        print("A empresa aérea não pode ser vazia.")
-
-        return False
-
-    logging.debug(f"Empresa aérea: {airline}")
-    if not departure:
-
-        logging.error("Origem vazia.")
-
-        print("A origem não pode ser vazia.")
-
-        return False
-
-    logging.debug(f"Origem: {departure}")
-    if not arrive:
-
-        logging.error("Destino vazio.")
-
-        print("O destino não pode ser vazio.")
-
-        return False
-
-    logging.debug(f"Destino: {arrive}")
-
-    if not flight_number:
-
-        logging.error("Número do voo vazio.")
-
-        print("O número do voo não pode ser vazio.")
-
-        return False
-
-    logging.debug(f"Número do voo: {flight_number}")
-
-    return True
-
-
-
-def validate_passengers(passengers):
-
-    try:        
-
-        if passengers <= 0:
-
-            logging.debug(f"Número de passageiros: {passengers}")
-
-            print("O número de passageiros deve ser positivo.")
-
-            return False
-
-    except ValueError:
-
-        logging.error(f"{passengers}, número de passageiros inválido.")
-
-        print("Número de passageiros inválido. Deve ser um número inteiro.")
-
-        return False
-
-    return True
-
+takeoff_queue = TakeoffQueue()
 
 def get_menu():
-    menu_options = {
+    menu_display_options = {
         1: "Adicionar um avião na fila de decolagem",
         2: "Permitir a decolagem do primeiro avião na fila",
         3: "Mostrar o total de aviões aguardando na fila de decolagem",
@@ -86,21 +13,17 @@ def get_menu():
         6: "Mostrar a posição de um avião conforme o número do voo",
         0: "Sair"
     }
-    return menu_options
-
-
-def get_menu():
-    menu_options = {
-        1: "Adicionar um avião na fila de decolagem",
-        2: "Permitir a decolagem do primeiro avião na fila",
-        3: "Mostrar o total de aviões aguardando na fila de decolagem",
-        4: "Listar todos os aviões na fila de decolagem",
-        5: "Listar as características do próximo a decolar",
-        6: "Mostrar a posição de um avião conforme o número do voo",
-        0: "Sair"
+    
+    function_options = {
+        1: takeoff_queue.add_flight,
+        2: takeoff_queue.takeoff_flight,
+        3: takeoff_queue.get_queue_size,
+        4: takeoff_queue.get_list_all_aircraft,
+        5: takeoff_queue.get_next_aircraft,
+        6: takeoff_queue.get_aircraft_position,
+        0: lambda: print("Encerrando o programa. Até mais!")
     }
-    return menu_options
-
+    return menu_display_options, function_options    
 
 def configure_logging():
     log_file_name = f"log_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
