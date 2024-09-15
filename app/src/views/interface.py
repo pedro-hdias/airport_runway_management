264bf4f31ee2851bada220e0a2ab88efef5159e9
pedro-hdias <pedrohenriquediassousa.2000@gmail.com>
2024-09-15 16:33:07 -0300

    Refatorar classe Aircrafts e adicionar logging e validação de entrada em helpers.py

--- app/src/views/interface.py
+++ app/src/views/interface.py
@@ -1,21 +1,26 @@
+import logging
+from utils.helpers import get_menu
+menu_options = get_menu()
+
 def show_menu():
-    print("\n===== Sistema de Gerenciamento de Fila de Decolagem =====")
-    print("Selecione uma opção:")
-    print("1. Adicionar um avião na fila de decolagem")
-    print("2. Permitir a decolagem do primeiro avião na fila")
-    print("3. Mostrar o total de aviões aguardando na fila de decolagem")
-    print("4. Listar todos os aviões na fila de decolagem")
-    print("5. Listar as características do próximo a decolar")
-    print("6. Mostrar a posição de um avião conforme o número do voo")
-    print("0. Sair")
+    logging.debug("Exibindo o menu.")
+    print("""\n===== Sistema de Gerenciamento de Fila de Decolagem =====\n
+          Selecione uma opção:""")
+    for key, value in menu_options.items():
+        logging.debug(f"{key}. {value}")
+        print(f"{key}. {value}")
 
 def get_user_option():
     try:
+        logging.debug("Obtendo a opção do usuário.")
         option = int(input("Digite o número da opção desejada: "))
         if option < 0 or option > 6:
-            print("Opção inválida! Por favor, insira um número entre 0 e 6.")
+            logging.warning(f"Opção inválida: {option}")
+            print(f"Opção inválida! Por favor, insira um dos números exibidos no menu.")
             return -1
+        logging.debug(f"Opção escolhida: {option}")
         return option
     except ValueError:
+        logging.warning(f"Opção inválida: {option}.")
         print("Opção inválida! Por favor, insira um número.")
         return -1
